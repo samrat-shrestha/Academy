4fc6169174ca72f5fb775f65988c8ccd2aa611eb
thebluess <samstha007@gmail.com>
2022-03-11 20:04:28 +0545

    Add project files.

---
+++ SikkaAcademy/Data/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SikkaAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SikkaAcademy.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
